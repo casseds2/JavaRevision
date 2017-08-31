cf3675b692b548b45f0fb14b60456c976b73a7d9
Stephen Cassedy <casseds95@gmail.com>
2017-08-31 11:55:34 +0100

    LeagueTable

--- ex8/LeagueTable.java
+++ ex8/LeagueTable.java
@@ -1,2 +1,20 @@
 import java.util.TreeMap;
+import java.util.Map;
 import java.util.Scanner;
+
+class LeagueTable{
+	public static void main(String [] args){
+
+		TreeMap<String, String> table = new TreeMap<>();
+		Scanner in = new Scanner(System.in);
+		String key = "";
+		String value = "";
+		while((key = in.next()) != null && !key.equals("0")){
+			if((value = in.next()) != null)
+				table.put(key, value);
+		}
+		for(Map.Entry<String, String> e : table.entrySet()){
+			System.out.println(e.getKey() + " : " + e.getValue());
+		}
+	}
+}
