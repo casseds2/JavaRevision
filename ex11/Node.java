1fda04aedcb95c1c7ee11c249b6c48941530ffd6
Stephen Cassedy <casseds95@gmail.com>
2017-09-07 11:19:00 +0100

    Node For Linked Stack

---
+++ ex11/Node.java
@@ -0,0 +1,8 @@
+class Node<T> {            
+	T item;    
+	Node<T> next = null;
+	                                                          
+	Node(T item0, Node<T> next0) { 
+		item = item0; next = next0;
+	}
+}
